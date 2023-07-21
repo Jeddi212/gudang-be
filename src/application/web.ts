5ac388c94c972ef47b1caf8043bdd32cc2754f10
Jeddi212 <jedediahf51@gmail.com>
2023-07-22 00:54:59 +0700

    🔐 :: private api

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { publicRouter } from '../route/public-api'
+import { privateRouter } from '../route/api'
 import { errorMiddleware } from '../middleware/error-middleware'
 
 export const app = express()
 app.use(express.json())
 app.use(publicRouter)
+app.use(privateRouter)
 app.use(errorMiddleware)
