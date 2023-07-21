0287ce05e73a428c1c17364b5920e812380352c0
Jeddi212 <jedediahf51@gmail.com>
2023-07-22 00:45:11 +0700

    🍮 :: add express json

--- src/application/web.ts
+++ src/application/web.ts
@@ -3,5 +3,6 @@ import { publicRouter } from '../route/public-api'
 import { errorMiddleware } from '../middleware/error-middleware'
 
 export const app = express()
+app.use(express.json())
 app.use(publicRouter)
 app.use(errorMiddleware)
