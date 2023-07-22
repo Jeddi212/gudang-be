ec39fe6955f634d26da6d1fcb00ce407c0403f23
Jeddi212 <jedediahf51@gmail.com>
2023-07-22 16:11:41 +0700

    🍩 :: error middleware

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,6 +1,13 @@
 import { ResponseError } from '../dto/response-error'
+import { Request, Response, NextFunction } from 'express'
 
-const errorMiddleware = async (err, _req, res, _next) => {
+declare global {
+    interface Error {
+        status?: number
+    }
+}
+
+function errorMiddleware(err: Error, _req: Request, res: Response, _next: NextFunction) {
     const status = err.status || 500
     const message = err.message || 'Internal Server Error'
 
