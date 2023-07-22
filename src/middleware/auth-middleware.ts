0a6cfa22d2b048559e723f78ecada7c1703a5fea
Jeddi212 <jedediahf51@gmail.com>
2023-07-22 16:11:52 +0700

    🌥️ :: auth middleware

---
+++ src/middleware/auth-middleware.ts
@@ -0,0 +1,36 @@
+import { JWTPayload } from '../dto/payload'
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const secretKey = process.env.JWT_SECRET_KEY || ''
+
+declare global {
+    namespace Express {
+        interface Request {
+            payload?: JWTPayload
+        }
+    }
+}
+
+function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.header('Authorization')?.replace('Bearer ', '')
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: Token not found' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey) as { payload: JWTPayload }
+        req.payload = decoded.payload
+        next()
+    } catch (err) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token' })
+    }
+}
+
+export {
+    authMiddleware
+}
