c9bdc9cc4a02975b41d821fa974d1a30a8dc6d35
Jeddi212 <jedediahf51@gmail.com>
2023-07-22 20:30:17 +0700

    🛼 :: add form middleware

--- src/application/web.ts
+++ src/application/web.ts
@@ -5,6 +5,7 @@ import { errorMiddleware } from '../middleware/error-middleware'
 
 export const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(publicRouter)
 app.use(privateRouter)
 app.use(errorMiddleware)
