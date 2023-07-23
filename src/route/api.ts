01f03ff2ddd44782a049462eed93aa21d54ac068
Jeddi212 <jedediahf51@gmail.com>
2023-07-23 22:08:58 +0700

    🍼 :: use authMiddleware

--- src/route/api.ts
+++ src/route/api.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import userController from '../controllers/user-controller'
+import { authMiddleware } from '../middleware/auth-middleware'
 
 const privateRouter = express.Router()
+privateRouter.use(authMiddleware)
 
 export {
     privateRouter
