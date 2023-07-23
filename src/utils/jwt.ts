5134048739e0c0c904104206326b6517c13cc294
Jeddi212 <jedediahf51@gmail.com>
2023-07-23 22:01:59 +0700

    🖱️ :: fix wrong class type

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,9 @@
-import { JWTPayload } from '../dto/payload'
+import { UserDTO } from '../dto/user-dto';
 import jwt from 'jsonwebtoken'
 
 const secretKey = process.env.JWT_SECRET_KEY || ''
 
-const generateJWT = (payload: JWTPayload) => {
+const generateJWT = (payload: UserDTO) => {
     return jwt.sign({ payload }, secretKey, {
         expiresIn: '1h',
     });
