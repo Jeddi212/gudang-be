f5fcc26f56c82e5b50882da67b300f61cc8d5e51
Jeddi212 <jedediahf51@gmail.com>
2023-07-23 20:53:38 +0700

    😿 :: login api

--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -4,6 +4,7 @@ import userController from '../controllers/user-controller'
 const publicRouter = express.Router()
 
 publicRouter.post('/register', userController.register)
+publicRouter.post('/login', userController.login)
 
 export {
     publicRouter
