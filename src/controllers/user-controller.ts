9c89f6a06bf50f84bb5eabe62d674ee5e1a77db6
Jeddi212 <jedediahf51@gmail.com>
2023-07-23 20:25:26 +0700

    ✏️ :: using JWTPayload

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,6 +1,5 @@
 import { Payload, JWTPayload } from '../dto/payload'
 import { UserAuthDTO } from '../dto/user-dto'
-import { UserModel } from '../models/user-model'
 import userService from '../services/user-service'
 import { Request, Response, NextFunction } from 'express'
 import validation from '../utils/validation'
@@ -11,7 +10,7 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
 
         const dto: UserAuthDTO = new UserAuthDTO(req.body.name, req.body.password)
 
-        const user: UserModel = await userService.register(dto)
+        const user: JWTPayload = await userService.register(dto)
         const payload: Payload = new Payload(
             'Register success',
             new JWTPayload(user.name, user.id, user.level)
