09a51c96131ae1436500e3587cddc34062ad4f6c
Jeddi212 <jedediahf51@gmail.com>
2023-07-29 18:21:12 +0700

    💫 :: cors option

--- src/utils/web.ts
+++ src/utils/web.ts
@@ -1,11 +1,18 @@
+import cors from 'cors';
 import express from 'express'
 import { publicRouter } from '../route/public-api'
 import { privateRouter } from '../route/api'
 import { errorMiddleware } from '../middleware/error-middleware'
 
+export const corsOption = {
+    origin: process.env.FRONTEND_LINK,
+    optionSuccessStatus: 200
+}
+
 export const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors(corsOption))
 app.use(publicRouter)
 app.use(privateRouter)
 app.use(errorMiddleware)
