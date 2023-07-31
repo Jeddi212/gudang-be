4639f6e790e27fd65b13badfdab2e4279897c1e7
Jeddi212 <jedediahf51@gmail.com>
2023-07-31 23:42:51 +0700

    🧩 :: index path

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,6 +3,6 @@ function logout() {
     const confirmLogout = window.confirm("Are you sure you want to logout?");
     if (confirmLogout) {
         document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        window.location.href = "/index.html";
+        window.location.href = "/";
     }
 }
