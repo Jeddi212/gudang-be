d4fd7db19fe7b77235c108ebfafa9a084b69eac1
Jeddi212 <jedediahf51@gmail.com>
2023-08-02 03:19:55 +0700

    🏏 :: read transactions view

--- src/controllers/view-controller.ts
+++ src/controllers/view-controller.ts
@@ -1,8 +1,8 @@
+import { Event } from '@prisma/client'
 import { Request, Response, NextFunction } from 'express'
-import { CreateProductDTO, Material } from '../dto/product-dto'
 import productService from '../services/product-service'
+import transactionService from '../services/transaction-service'
 import validation from '../utils/validation'
-
 
 const index = async (_req: Request, res: Response) => {
     res.render('index', {
@@ -43,9 +43,39 @@ const product = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+const transaction = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.render('transaction', {
+            title: 'Gudang | Transaction',
+            layout: './layouts/main-layout.ejs'
+        })
+    } catch (e) {
+        next(e)
+    }
+}
+
+const transactionData = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const event = req.query.event as Event || ''
+        const username = req.query.username as string || ''
+
+        let transactions = await transactionService.findTransactions(event, username)
+
+        res.render('transaction-data', {
+            transactions,
+            title: 'Gudang | Transaction',
+            layout: './layouts/plain-layout.ejs'
+        })
+    } catch (e) {
+        next(e)
+    }
+}
+
 export default {
     index,
     login,
     register,
     product,
+    transaction,
+    transactionData,
 }
--- src/route/routing.ts
+++ src/route/routing.ts
@@ -8,6 +8,8 @@ viewRouter.get('/login', viewController.login)
 viewRouter.get('/register', viewController.register)
 
 viewRouter.get('/product', viewController.product)
+viewRouter.get('/transaction', viewController.transaction)
+viewRouter.get('/transaction-data', viewController.transactionData)
 
 export {
     viewRouter
