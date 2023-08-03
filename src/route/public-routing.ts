3e3fec856e3f2cf60eb0bf9d891f7fdf61bbf12a
Jeddi212 <jedediahf51@gmail.com>
2023-08-03 11:59:37 +0700

    🎂 :: guest middleware

--- src/route/public-routing.ts
+++ src/route/public-routing.ts
@@ -5,21 +5,20 @@ import { guestAuthMiddleware } from '../middleware/auth-middleware'
 const publicRouter = express.Router()
 
 publicRouter.get('/', viewController.index)
-publicRouter.use(guestAuthMiddleware)
 
-publicRouter.get('/menu', viewController.menu)
-publicRouter.get('/login', viewController.login)
-publicRouter.get('/register', viewController.register)
+publicRouter.get('/menu', guestAuthMiddleware, viewController.menu)
+publicRouter.get('/login', guestAuthMiddleware, viewController.login)
+publicRouter.get('/register', guestAuthMiddleware, viewController.register)
 
-publicRouter.get('/warehouse', viewController.warehouse)
-publicRouter.get('/warehouse/:location', viewController.warehouseDetail)
+publicRouter.get('/warehouse', guestAuthMiddleware, viewController.warehouse)
+publicRouter.get('/warehouse/:location', guestAuthMiddleware, viewController.warehouseDetail)
 
-publicRouter.get('/product', viewController.product)
-publicRouter.get('/product/:name', viewController.productDetail)
+publicRouter.get('/product', guestAuthMiddleware, viewController.product)
+publicRouter.get('/product/:name', guestAuthMiddleware, viewController.productDetail)
 
-publicRouter.get('/transaction', viewController.transaction)
-publicRouter.get('/transaction/:id', viewController.transactionDetail)
-publicRouter.get('/transaction-data', viewController.transactionData)
+publicRouter.get('/transaction', guestAuthMiddleware, viewController.transaction)
+publicRouter.get('/transaction/:id', guestAuthMiddleware, viewController.transactionDetail)
+publicRouter.get('/transaction-data', guestAuthMiddleware, viewController.transactionData)
 
 publicRouter.get('/inventory', guestAuthMiddleware, viewController.inventory)
 
